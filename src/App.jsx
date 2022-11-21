c7b65d5662a80092fca22c2f7d4eb4cd10f2ef8b
Vitor Galdino <httpvittor@gmail.com>
2022-11-21 19:14:10 -0300

    feat: created useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+
+import { Header } from './components/Header';
+import { instance } from './services/api';
+
+import './App.css';
 
 function App() {
 
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [currentSale, setCurrentSale] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+
+  useEffect(() => {
+    instance.get('/products')
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+
   return (
     <div className="App">
-      
+      <Header />
+      <main>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
