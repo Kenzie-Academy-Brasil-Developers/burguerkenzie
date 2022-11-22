0ba969eb169fa10ec62772c1338ca4bdd82899e8
Vitor Galdino <httpvittor@gmail.com>
2022-11-22 14:08:23 -0300

    refactor: remove tests img

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import testImg from '../../assets/soOUeeW.png';
+
 import { ProductContent, ProductInfo } from './styles';
 
 export const Product = ({ product }) => {
     return (
         <ProductContent>
-            {/* <img draggable='false' src={product.img} alt={product.name} /> */}
             <div className='img-box'>
-                <img draggable='false' src={testImg} alt={product.name} />
+                <img draggable='false' src={product.img} alt={product.name} />
             </div>
             <ProductInfo>
                 <h3>{product.name}</h3>
