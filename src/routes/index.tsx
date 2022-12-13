c6ab947288cb406024dcfd0809e63670fcceec1d
Vitor Galdino <httpvittor@gmail.com>
2022-12-12 23:46:01 -0300

    feat: created Routes component with react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,22 @@
-import { Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import { CartProvider } from '../contexts/CartContext';
+import { Home } from '../pages/Home';
+import { Login } from '../pages/Login';
+import { Register } from '../pages/Register';
 
 export const MainRoutes = () => {
     return (
         <Routes>
-            
+            <Route path='' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/home' element={
+                <CartProvider>
+                    <Home />
+                </CartProvider>
+            }
+            />
+            <Route path='*' element={<Navigate to='' />} />
         </Routes>
     );
 };
