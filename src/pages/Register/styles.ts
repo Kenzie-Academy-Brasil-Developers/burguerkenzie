f7cb3a14d0e8f68b291576f68629f7c7630563e4
Vitor Galdino <httpvittor@gmail.com>
2022-12-14 17:30:49 -0300

    feat: created UserContainer with styled components

---
+++ src/pages/Register/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const UserContainer = styled.main`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100vh;
+    width: 100%;
+    max-width: 1023px;
+    padding: 0 .9375rem;
+    margin: auto;
+`;
