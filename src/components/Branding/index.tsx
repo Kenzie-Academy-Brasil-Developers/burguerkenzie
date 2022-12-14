ee04e9666795138a8b21c7db192dbfc93f960ff4
Vitor Galdino <httpvittor@gmail.com>
2022-12-14 16:54:13 -0300

    feat: creatd Branding component

---
+++ src/components/Branding/index.tsx
@@ -0,0 +1,9 @@
+import branding from '../../assets/branding.svg';
+
+export const Branding = () => {
+    return (
+        <>
+            <img draggable='false' src={branding} alt='Burguer Kenzie' />
+        </>
+    );
+};
