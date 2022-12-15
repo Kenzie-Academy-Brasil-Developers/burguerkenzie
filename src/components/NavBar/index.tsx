4fc1974696dac07a892ab249508444f400bfb2be
Vitor Galdino <httpvittor@gmail.com>
2022-12-15 19:12:48 -0300

    feat: included CartIcon, LogoutIcon and palette with material-ui

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -5,7 +5,34 @@ import { Button } from '../../styles/buttons';
 import { DefaultInput } from '../../styles/inputs';
 import { NavBarContainer } from './styles';
 
+import LogoutTwoToneIcon from '@mui/icons-material/LogoutTwoTone';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Badge, { BadgeProps } from '@mui/material/Badge';
+import IconButton from '@mui/material/IconButton';
+import { createTheme, styled } from '@mui/material/styles';
+
 import searchImage from '../../assets/search.svg';
+
+const CartTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#27AE60',
+        },
+        secondary: {
+            main: '#BDBDBD',
+        }
+    },
+});
+
+const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
+    '& .MuiBadge-badge': {
+        right: -3,
+        top: 11,
+        border: `2px solid ${theme.palette.background.paper}`,
+        padding: '0 4px',
+        background: CartTheme.palette.primary.main,
+    },
+}));
 
 interface IInputSearchProps {
     onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
@@ -24,6 +51,20 @@ export const NavBar = ({ onClick }: IInputSearchProps) => {
                     onChange={(e) => !e.target.value.length && setFilteredProducts([])}
                 />
                 <Button onClick={onClick} variant='mediumGreen'><img src={searchImage} /> </Button>
+            </li>
+            <li>
+                <IconButton aria-label='cart' >
+                    <StyledBadge badgeContent={3} sx={{ color: CartTheme.palette.secondary.main }} color='success'>
+                        <ShoppingCartIcon />
+                    </StyledBadge>
+                </IconButton>
+            </li>
+            <li>
+                <IconButton aria-label='exit' >
+                    <StyledBadge sx={{ color: CartTheme.palette.secondary.main }}>
+                        <LogoutTwoToneIcon />
+                    </StyledBadge>
+                </IconButton>
             </li>
         </NavBarContainer>
     );
