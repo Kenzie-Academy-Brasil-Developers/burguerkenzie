4c1c7163b82fb96be947648e1f26f1e15726066a
Vitor Galdino <httpvittor@gmail.com>
2022-12-15 16:37:43 -0300

    feat: included user route validation

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,17 @@
+import { useContext, useEffect } from 'react';
+
 import { Branding } from '../../components/Branding';
 import { FormLogin } from '../../components/Form/FormLogin';
+import { UserContext } from '../../contexts/UserContext';
 import { UserContainer } from './styles';
 
 export const Login = () => {
+    const { navigate } = useContext(UserContext);
+
+    useEffect(() => {
+        !!localStorage.userToken && navigate('/home');
+    }, []);
+
     return (
         <UserContainer>
             <FormLogin />
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,8 +1,17 @@
+import { useContext, useEffect } from 'react';
+
 import { Branding } from '../../components/Branding';
 import { FormRegister } from '../../components/Form/FormRegister';
+import { UserContext } from '../../contexts/UserContext';
 import { UserContainer } from './styles';
 
 export const Register = () => {
+    const { navigate } = useContext(UserContext);
+
+    useEffect(() => {
+        !!localStorage.userToken && navigate('/home');
+    }, []);
+
     return (
         <UserContainer>
             <Branding />
