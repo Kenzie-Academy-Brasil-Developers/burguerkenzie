ad0cfcaa6938a8b390a5770c9c0ed95579ccdefd
Vitor Galdino <httpvittor@gmail.com>
2022-12-15 16:05:18 -0300

    feat: created form validation

--- src/components/Form/FormLogin/index.tsx
+++ src/components/Form/FormLogin/index.tsx
@@ -1,20 +1,54 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import { TextField } from '@mui/material';
-import { Button, SecondaryLink } from '../../../styles/buttons';
+import { useContext } from 'react';
+import { useForm } from 'react-hook-form';
 
+import { UserContext } from '../../../contexts/UserContext';
+import { Button, SecondaryLink } from '../../../styles/buttons';
+import { loginSchema } from './loginSchema';
 import { FormLoginContainer } from './styles';
 
+export interface ILoginForm {
+    email: string;
+    password: string;
+}
+
 export const FormLogin = () => {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<ILoginForm>({
+        mode: 'onChange',
+        resolver: yupResolver(loginSchema)
+    });
+
+    const { loginRequest } = useContext(UserContext);
+
+    const sendToApi = (data: ILoginForm) => loginRequest(data, reset);
+
     return (
         <FormLoginContainer>
             <div>
                 <h4>Login</h4>
             </div>
-            <form noValidate>
-                <TextField label="Email" variant="outlined" fullWidth />
-                <TextField type='password' label="Senha" variant="outlined" fullWidth />
+            <form noValidate onSubmit={handleSubmit(sendToApi)}>
+                <TextField
+                    label="Email"
+                    variant="outlined"
+                    fullWidth
+                    {...register('email')}
+                    helperText={errors.email?.message}
+                    error={!!errors.email}
+                />
+                <TextField
+                    type='password'
+                    label="Senha"
+                    variant="outlined"
+                    fullWidth
+                    {...register('password')}
+                    helperText={errors.password?.message}
+                    error={!!errors.password}
+                />
                 <Button type='submit' variant='defaultGreen'>Logar</Button>
             </form>
-            <p>Crie sua conta para saborear muitas delícias e matar sua fome!</p>        
+            <p>Crie sua conta para saborear muitas delícias e matar sua fome!</p>
             <SecondaryLink to='/register'>Cadastrar</SecondaryLink>
         </FormLoginContainer>
     );
