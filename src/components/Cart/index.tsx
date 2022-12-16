9b3c64d986e3391d598b31ba301b19e521b4fb52
Vitor Galdino <httpvittor@gmail.com>
2022-12-15 21:47:05 -0300

    refactor: change aside to be modal

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -5,8 +5,11 @@ import { CartProduct } from '../CartProduct';
 import { CartTotal } from '../CartTotal';
 import { CartBox, CartContent, CartHeader } from './styles';
 
+import CloseTwoToneIcon from '@mui/icons-material/CloseTwoTone';
+import { IconButton } from '@mui/material';
+
 export const Cart = () => {
-    const { currentSale, setCurrentSale, setTotalItemCart } = useContext(CartContext);
+    const { currentSale, setCurrentSale, setTotalItemCart, setIsOpenCart } = useContext(CartContext);
 
     const removeProduct = (e: React.MouseEvent<HTMLButtonElement>) => {
         const newList = currentSale.filter((elem) => parseInt(e.currentTarget.id) !== elem.id);
@@ -16,27 +19,40 @@ export const Cart = () => {
 
     return (
         <CartBox>
-            <CartHeader>Carrinho de compras</CartHeader>
-            {!currentSale.length
-                ? <CartContent heightSize='158px'>
-                    <h4>Sua sacola está vazia</h4>
-                    <span>Adicione itens</span>
-                </CartContent>
-                : <CartContent heightSize='305px'>
-                    <ul>
-                        {currentSale.map((product) => {
-                            return (
-                                <CartProduct
-                                    data={product}
-                                    onClick={removeProduct}
-                                    key={product.id}
-                                />
-                            );
-                        })}
-                    </ul>
-                </CartContent>
-            }
-            {!!currentSale.length && <CartTotal />}
+            <div>
+                <CartHeader>
+                    <h3>Carrinho de compras</h3>
+                    <IconButton
+                        aria-label='close-modal'
+                        sx={
+                            { '&:hover': { backgroundColor: '#ffffff1c' } }
+                        }
+                        onClick={() => setIsOpenCart(false)}
+                    >
+                        <CloseTwoToneIcon sx={{ color: 'rgba(255, 255, 255, 0.5)' }} />
+                    </IconButton>
+                </CartHeader>
+                {!currentSale.length
+                    ? <CartContent heightSize='158px'>
+                        <h4>Sua sacola está vazia</h4>
+                        <span>Adicione itens</span>
+                    </CartContent>
+                    : <CartContent heightSize='305px'>
+                        <ul>
+                            {currentSale.map((product) => {
+                                return (
+                                    <CartProduct
+                                        data={product}
+                                        onClick={removeProduct}
+                                        key={product.id}
+                                    />
+                                );
+                            })}
+                        </ul>
+                    </CartContent>
+                }
+                {!!currentSale.length && <CartTotal />}
+            </div>
         </CartBox>
     );
 };
