f0629d65c9d9f8b5e171a3d9299a6f342c679188
Vitor Galdino <httpvittor@gmail.com>
2022-12-17 18:25:11 -0300

    feat: created search-box id style

--- src/components/ExpandInput/styles.ts
+++ src/components/ExpandInput/styles.ts
@@ -1,4 +1,4 @@
-import { TextField } from '@mui/material';
+import { IconButton as IButton, TextField as Input } from '@mui/material';
 import styled from 'styled-components';
 
 interface IInputProps {
@@ -17,12 +17,21 @@ export const InputContainer = styled.div<IInputProps>`
     input {
         padding-right: 48px;
     }
+
+    #search-box {
+        position: ${({ isExpanded }) => isExpanded ? 'absolute' : 'static'};
+        pointer-events: ${({ isExpanded }) => isExpanded ? 'none' : 'auto'};
+    }
 `;
 
-export const Input = styled(TextField) <IInputProps>`
+export const TextField = styled(Input) <IInputProps>`
     margin: 0;
     width: ${({ isExpanded }) => isExpanded ? '100%' : '0%'};
     pointer-events: ${({ isExpanded }) => isExpanded ? 'auto' : 'none'};
     opacity: ${({ isExpanded }) => isExpanded ? '1' : '0'};
     transition: .8s cubic-bezier(.47, .4, .175, 1.2);
+`;
+
+export const IconButton = styled(IButton) <IInputProps>`
+
 `;
