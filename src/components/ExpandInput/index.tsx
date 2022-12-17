290c94073559034d12eebf69676f68a8d59e8c2c
Vitor Galdino <httpvittor@gmail.com>
2022-12-17 18:16:30 -0300

    refactor: remove style inline on Input

--- src/components/ExpandInput/index.tsx
+++ src/components/ExpandInput/index.tsx
@@ -1,7 +1,7 @@
 import SearchIcon from '@mui/icons-material/Search';
-import { createTheme, IconButton, InputAdornment, TextField } from '@mui/material';
+import { createTheme, IconButton, InputAdornment } from '@mui/material';
 import { useEffect, useState } from 'react';
-import { InputContainer } from './styles';
+import { Input, InputContainer } from './styles';
 
 const CartTheme = createTheme({
     palette: {
@@ -30,26 +30,16 @@ export const ExpandInput = () => {
     return (
         <InputContainer isExpanded={isExpanded}>
             <div>
-                <TextField
+                <Input
                     id="outlined-full-width"
-                    style={{
-                        margin: 0,
-                        width: isExpanded ? '100%' : '0%',
-                        pointerEvents: isExpanded ? 'auto' : 'none',
-                        opacity: isExpanded ? '1' : '0',
-                        transition: 'all .8s cubic-bezier(.47, .4, .175, 1.2)'
-                    }}
+                    isExpanded={isExpanded}
                     placeholder="Buscar"
                     fullWidth
-                    margin="normal"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
                     variant="outlined"
                 />
                 <InputAdornment position="start" >
                     <IconButton id='search-box' style={{ position: isExpanded ? 'absolute' : 'static', pointerEvents: isExpanded ? 'none' : 'auto' }} onClick={() => setIsExpanded(!isExpanded)}>
-                        <SearchIcon id='search-icon' style={{ position: isExpanded ? 'absolute' : 'static', right: '35px', color: '#BDBDBD' }}  />
+                        <SearchIcon id='search-icon' style={{ position: isExpanded ? 'absolute' : 'static', right: '35px', color: '#BDBDBD' }} />
                     </IconButton>
                 </InputAdornment>
             </div>
