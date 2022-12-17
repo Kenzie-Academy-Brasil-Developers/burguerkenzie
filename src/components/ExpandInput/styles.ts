07bde5c99f7e407ed3aae729274ae9c8601d6042
Vitor Galdino <httpvittor@gmail.com>
2022-12-17 18:15:49 -0300

    feat: created Input with styled-components

--- src/components/ExpandInput/styles.ts
+++ src/components/ExpandInput/styles.ts
@@ -1,3 +1,4 @@
+import { TextField } from '@mui/material';
 import styled from 'styled-components';
 
 interface IInputProps {
@@ -16,4 +17,12 @@ export const InputContainer = styled.div<IInputProps>`
     input {
         padding-right: 48px;
     }
+`;
+
+export const Input = styled(TextField) <IInputProps>`
+    margin: 0;
+    width: ${({ isExpanded }) => isExpanded ? '100%' : '0%'};
+    pointer-events: ${({ isExpanded }) => isExpanded ? 'auto' : 'none'};
+    opacity: ${({ isExpanded }) => isExpanded ? '1' : '0'};
+    transition: .8s cubic-bezier(.47, .4, .175, 1.2);
 `;
