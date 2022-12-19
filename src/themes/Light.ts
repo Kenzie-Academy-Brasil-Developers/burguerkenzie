4d2aace4d3c4bc3fa7790e20f9d5a4d5c694d918
Vitor Galdino <httpvittor@gmail.com>
2022-12-19 17:08:19 -0300

    feat: created Light theme to material ui components

---
+++ src/themes/Light.ts
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material';
+
+export const LightTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#27AE60',
+            dark: '#219653',
+            light: '#93D7AF',
+        },
+        secondary: {
+            main: '#828282',
+            dark: '#333333',
+            light: '#E0E0E0', 
+        }
+    }
+});
